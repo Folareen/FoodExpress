8c7db14107fbfc7f864f750a9611505326d3b773
Folareen <sakawahab03@gmail.com>
2023-06-05 11:11:43 +0100

    ..

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Landing from './src/screens/Landing';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>FoodExpress ddddddddd</Text>
+    <Landing />
+  )
+
+  return (
+    <View className='flex-1 w-full bg-red-200 items-center justify-center '>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
