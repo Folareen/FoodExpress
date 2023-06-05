7a4103f35b9cc3f11931685d19d6c3aae2e0760b
Folareen <sakawahab03@gmail.com>
2023-06-05 15:04:21 +0100

    changed fonts to ubuntu

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,34 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import Landing from './src/screens/Landing';
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 
+  const [isLoaded] = useFonts({
+    "normal": require("./src/assets/fonts/Ubuntu-Light.ttf"),
+    "bold": require("./src/assets/fonts/Ubuntu-Medium.ttf"),
+    "extrabold": require("./src/assets/fonts/Ubuntu-Bold.ttf"),
+  });
+
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <Landing />
+    <View onLayout={handleOnLayout} className='flex-1'>
+      <Landing />
+    </View>
   )
 
   return (
--- app/src/screens/Landing.tsx
+++ app/src/screens/Landing.tsx
@@ -7,15 +7,15 @@ const Landing = () => {
         <View className='bg-[#FF4B3A] flex-1'>
             <StatusBar style="light" />
             <View className='bg-white rounded-full self-start mt-[67px] ml-[62px] w-[73px] h-[73px] items-center justify-center'>
-                <Image source={require('../assets/logo.png')} className=' w-[45px] h-[45px]' />
+                <Image source={require('../assets/images/logo.png')} className=' w-[45px] h-[45px]' />
             </View>
-            <Text className='text-[65px] text-white ml-[62px] font-extrabold'>
+            <Text className='text-[65px] !leading-[0] text-white ml-[62px] font-[extrabold]'>
                 Food for
                 Everyone
             </Text>
-            <Image source={require('../assets/landing-main.png')} className=' ' />
+            <Image source={require('../assets/images/landing-main.png')} className=' ' />
             <TouchableOpacity className='bg-white rounded-[30px] absolute bottom-4 left-[10%] right-[10%] py-[25px] px-[100px] w-4/5'>
-                <Text className='text-[#FF460A] text-[16px] font-bold'>
+                <Text className='text-[#FF460A] text-[16px] font-[normal]'>
                     Get Started
                 </Text>
             </TouchableOpacity>
