f1e327d60fa6a2217e7c0dd2f4027a44a8de6c8b
Folareen <sakawahab03@gmail.com>
2023-06-05 18:15:11 +0100

    done with landing screen..

--- app/src/screens/Landing.tsx
+++ app/src/screens/Landing.tsx
@@ -2,7 +2,8 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 
-const Landing = () => {
+const Landing = ({ navigation }: { navigation: any }) => {
+
     return (
         <View className='bg-[#FF4B3A] flex-1'>
             <StatusBar style="light" />
@@ -14,8 +15,12 @@ const Landing = () => {
                 Everyone
             </Text>
             <Image source={require('../assets/images/landing-main.png')} className=' ' />
-            <TouchableOpacity className='bg-white rounded-[30px] absolute bottom-4 left-[10%] right-[10%] py-[25px] px-[100px] w-4/5'>
-                <Text className='text-[#FF460A] text-[16px] font-[normal]'>
+            <TouchableOpacity className='bg-white rounded-[30px] absolute bottom-4 left-[10%] right-[10%] py-[20px]  w-4/5 items-center' onPress={
+                () => {
+                    navigation.navigate('home')
+                }
+            }>
+                <Text className='text-[#FF460A] text-[16px] font-[bold]'>
                     Get Started
                 </Text>
             </TouchableOpacity>
