90b617742d2c8f6fd6f93d74135b3f8987703034
Folareen <sakawahab03@gmail.com>
2023-06-05 23:55:13 +0100

    navigation..

---
+++ app/src/Navigation.tsx
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Landing from './screens/Landing'
+import Auth from './screens/Auth'
+import Home from './screens/Home'
+import Search from './screens/Search'
+
+const Stack = createNativeStackNavigator()
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator >
+                <Stack.Screen name='landing' component={Landing} options={{ headerShown: false }} />
+                <Stack.Screen name='home' component={Home} options={{ headerShown: false }} />
+                <Stack.Screen name='search' component={Search} options={{ headerShown: false }} />
+                <Stack.Screen name='auth' component={Auth} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Navigation
