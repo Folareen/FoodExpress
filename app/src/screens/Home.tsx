c1b5c3d6d84b917b6f5996110d42e83f5895366f
Folareen <sakawahab03@gmail.com>
2023-06-06 18:09:14 +0100

    fetched and displayed products from sanity

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,13 +1,42 @@
 import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Entypo, Feather, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import ProductCard from '../components/ProductCard';
-
-const categories = ['foods', 'drinks', 'snacks', 'sauces', 'vegans']
+import useSanityFetch from '../hooks/useSanityFetch';
+import { FlatList } from 'react-native';
+import { urlFor } from '../config/sanityClient';
+import { Skeleton } from '@rneui/themed';
+import { Dimensions } from 'react-native';
 
 const Home = ({ navigation }: { navigation: any }) => {
-    const [category, setCategory] = useState<string>('foods')
+    const [category, setCategory] = useState<{ title: string, _id: string } | null>(null)
+
+    const categories = useSanityFetch(`*[_type == "category"]
+        | order(_createdAt asc){
+            title,
+            _id
+        }
+    `, [])
+
+
+    const { data, loading, error } = useSanityFetch(`
+        *[_type == "product" && category._ref == "${category?._id}" ]{
+                    title,
+                    slug,
+                    cover_image,
+                    images,
+                    price,
+                }
+    `, [category])
+
+    useEffect(() => {
+        if (categories.data?.length > 0) {
+            setCategory(categories.data[0])
+        }
+    }, [categories.data])
+
+
 
     return (
         <View className='pt-[50px] pl-[50px] pb-[40px] flex-1 bg-[#EDEDED]'>
@@ -38,53 +67,55 @@ const Home = ({ navigation }: { navigation: any }) => {
             </TouchableOpacity>
 
             <View>
-                <ScrollView className='flex flex-row mt-[36px]
+                {
+                    categories.loading ?
+                        <Skeleton animation="wave" width={Dimensions.get('screen').width} height={40} style={{ marginTop: 36 }} />
+                        :
+                        <ScrollView className='flex flex-row mt-[36px]
                 ' horizontal={true} showsHorizontalScrollIndicator={false}>
-                    {
-                        categories.map((cgy) => (
-                            <TouchableOpacity onPress={() => {
-                                setCategory(cgy)
-                            }} className={`px-[20px] py-[10px] ${category == cgy ? 'border-b-[3px] border-solid border-primary' : 'border-0'}`}>
-                                <Text className={`capitalize  ${category == cgy ? 'text-primary' : 'text-[#9A9A9D]'} font-[normal] text-[17px] `}>
-                                    {cgy}
-                                </Text>
-                            </TouchableOpacity>
-                        ))
-                    }
-                </ScrollView>
+                            {
+                                categories.data?.map((cgy) => (
+                                    <TouchableOpacity onPress={() => {
+                                        setCategory(cgy)
+                                    }} className={`px-[20px] py-[10px] ${category?.title == cgy.title ? 'border-b-[3px] border-solid border-primary' : 'border-0'}`}>
+                                        <Text className={`capitalize  ${category?.title == cgy.title ? 'text-primary' : 'text-[#9A9A9D]'} font-[normal] text-[17px] `}>
+                                            {cgy.title}
+                                        </Text>
+                                    </TouchableOpacity>
+                                ))
+                            }
+                        </ScrollView>
+                }
+
             </View>
 
-            <View>
-                <ScrollView className='flex flex-row mt-[24px] -ml-[30px]' horizontal={true} showsHorizontalScrollIndicator={false}>
-                    {
-                        [
-                            {
-                                img: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80',
-                                title: 'Veggie tomato mix',
-                                price: 1900
-                            },
-                            {
-                                img: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80',
-                                title: 'Veggie tomato mix',
-                                price: 1900
-                            },
-                            {
-                                img: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80',
-                                title: 'Veggie tomato mix',
-                                price: 1900
-                            },
-                            {
-                                img: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80',
-                                title: 'Veggie tomato mix',
-                                price: 1900
-                            }
-                        ]
-                            .map(({ img, title, price }) => (
-                                <ProductCard navigation={navigation} title={title} price={price} img={img} isSearchItem={false} />
-                            ))
-                    }
-                </ScrollView>
-            </View>
+            {
+                loading ?
+                    <View>
+
+                        <ScrollView horizontal style={{ marginTop: 24, marginLeft: -30, flexDirection: 'row' }}>
+                            <Skeleton animation="wave" width={Dimensions.get('screen').width / 2} height={250} style={{ borderRadius: 40 }} />
+                            <Skeleton animation="wave" width={Dimensions.get('screen').width / 2} height={250} style={{ marginLeft: 20, borderRadius: 40 }} />
+                        </ScrollView>
+                    </View>
+                    :
+                    <FlatList
+                        className={`mt-[24px] -ml-[30px] ${data.length > 0 ? 'flex flex-row ' : ''}`}
+                        data={data}
+                        keyExtractor={({ slug }) => slug}
+                        renderItem={({ item: { title, slug, cover_image, images, price } }) => (
+                            <ProductCard navigation={navigation} title={title} price={price} img={urlFor(cover_image?.asset).url()} images={images} isSearchItem={false} slug={slug} />
+                        )}
+                        ListEmptyComponent={() => (
+                            <View className='!flex-1 items-center justify-center h-[200px] '>
+                                <Text className='text-[28px]'>
+                                    No item found
+                                </Text>
+                            </View>
+                        )}
+                    />
+            }
+
 
             <View className='mt-auto flex-row justify-between pr-[50px]'>
                 <TouchableOpacity>
