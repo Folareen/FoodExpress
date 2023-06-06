35a49cdb8a7f1cb138578b3c9ce116d38e9ef4ef
Folareen <sakawahab03@gmail.com>
2023-06-06 21:45:38 +0100

    added redux store

--- app/App.tsx
+++ app/App.tsx
@@ -4,6 +4,8 @@ import * as SplashScreen from 'expo-splash-screen'
 import { useCallback } from 'react';
 import Navigation from './src/Navigation';
 import 'react-native-url-polyfill/auto';
+import { Provider } from 'react-redux'
+import store from './src/redux/store';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -26,8 +28,11 @@ export default function App() {
   }
 
   return (
-    <View onLayout={handleOnLayout} className='flex-1'>
-      <Navigation />
-    </View>
+    <Provider store={store} >
+      <View onLayout={handleOnLayout} className='flex-1'>
+        <Navigation />
+      </View>
+    </Provider>
+
   )
 }
