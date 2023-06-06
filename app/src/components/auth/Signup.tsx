0549a3b5c192cbfcc82120322115d4540fc58801
Folareen <sakawahab03@gmail.com>
2023-06-06 22:13:53 +0100

    added more fields

--- app/src/components/auth/Signup.tsx
+++ app/src/components/auth/Signup.tsx
@@ -5,6 +5,8 @@ const Signup = () => {
     const [name, setName] = useState<string>('')
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
+    const [phoneNumber, setPhoneNumber] = useState<number>(0)
+    const [address, setAddress] = useState<string>('')
 
     return (
         <View className='px-[50px] pt-[50px] pb-[40px] flex-1'>
@@ -30,7 +32,18 @@ const Signup = () => {
                 } />
             </View>
 
-            <View>
+            <View className='mb-[50px]'>
+                <Text className='font-[bold] font-[15px]' style={{ color: 'rgba(0,0,0,0.4)' }}>
+                    Phone number
+                </Text>
+                <TextInput value={phoneNumber} onChangeText={
+                    (text) => {
+                        setPhoneNumber(Number(text))
+                    }
+                } className='border-b-[0.5px] border-solid border-black py-[4px] font-bold text-black text-[18px]' keyboardType='numeric' />
+            </View>
+
+            <View className='mb-[50px]'>
                 <Text className='font-[bold] font-[15px]' style={{ color: 'rgba(0,0,0,0.4)' }}>
                     Password
                 </Text>
@@ -39,6 +52,17 @@ const Signup = () => {
                         setPassword(text)
                     }
                 } className='border-b-[0.5px] border-solid border-black py-[4px] font-bold text-black text-[18px]' secureTextEntry />
+            </View>
+
+            <View>
+                <Text className='font-[bold] font-[15px]' style={{ color: 'rgba(0,0,0,0.4)' }}>
+                    House address
+                </Text>
+                <TextInput value={address} onChangeText={
+                    (text) => {
+                        setAddress(text)
+                    }
+                } className='border-b-[0.5px] border-solid border-black py-[4px] font-bold text-black text-[18px]' multiline={true} numberOfLines={3} style={{ textAlignVertical: 'top' }} />
             </View>
 
             <TouchableOpacity className='bg-primary rounded-[30px]  py-[20px] w-full mt-[80px] items-center ' onPress={
