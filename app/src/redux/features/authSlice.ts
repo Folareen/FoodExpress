932446d2fc7b1174811cafe6ec7a138a7aa807d7
Folareen <sakawahab03@gmail.com>
2023-06-07 16:28:41 +0100

    added notification for logout

--- app/src/redux/features/authSlice.ts
+++ app/src/redux/features/authSlice.ts
@@ -1,19 +1,30 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import Toast from 'react-native-root-toast'
 
-const initialState = {user: null}
+const initialState = { user: null }
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        authenticate : (state, action) => {
+        authenticate: (state, action) => {
             state.user = action.payload
         },
         logout: (state) => {
             state.user = null
+            Toast.show(`Signed out!`, {
+                duration: Toast.durations.SHORT,
+                position: 60,
+                shadow: true,
+                animation: true,
+                hideOnPress: true,
+                delay: 0,
+                backgroundColor: 'red',
+                textColor: '#fff'
+            });
         }
     }
 })
 
-export const {authenticate, logout} = authSlice.actions
+export const { authenticate, logout } = authSlice.actions
 export default authSlice.reducer
