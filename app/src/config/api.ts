d83fb99bb2f7a5953a6e29f364af0248e872ae44
Folareen <sakawahab03@gmail.com>
2023-06-07 13:12:27 +0100

    fixed axios token error

--- app/src/config/api.ts
+++ app/src/config/api.ts
@@ -4,19 +4,23 @@ const BASE_URL = 'http://192.168.231.156:5000/api/v1'
 
 const api = axios.create({
     baseURL: BASE_URL,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json;charse=UTF-8'
-    },
     timeout: 8000
 })
 
-export const setAxiosToken = (token: string) => {
-    if (token) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    } else {
-        delete axios.defaults.headers.common['Authorization'];
+export const setAxiosToken = (token : string) => {
+
+  return (api.interceptors.request.use(
+    async (config) => {
+      config.headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      return config;
+    },
+    (error) => {
+      Promise.reject(error);
     }
+  ))
 }
+
 
 export default api
