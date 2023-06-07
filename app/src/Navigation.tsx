9403addb82c86fb08b0ee2656ad85617b303af3b
Folareen <sakawahab03@gmail.com>
2023-06-07 06:46:13 +0100

    authenticate user with stored token

--- app/src/Navigation.tsx
+++ app/src/Navigation.tsx
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Landing from './screens/Landing'
@@ -9,10 +9,27 @@ import Search from './screens/Search'
 import ProductDetails from './screens/ProductDetails'
 import Cart from './screens/Cart'
 import Account from './screens/account'
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import jwtDecode from 'jwt-decode'
+import { setAxiosToken } from './config/api'
+import { useDispatch } from 'react-redux'
+import { authenticate } from './redux/features/authSlice'
 
 const Stack = createNativeStackNavigator()
 
 const Navigation = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        (async () => {
+            const token: string = await AsyncStorage.getItem('token')
+            if (Boolean(!token)) return
+            const user = await jwtDecode(token)
+            await AsyncStorage.setItem('token', token)
+            setAxiosToken(token)
+            dispatch(authenticate(user))
+        })()
+    }, [])
+
     return (
         <NavigationContainer>
             <Stack.Navigator >
