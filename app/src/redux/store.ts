8ad081b9844a2fbc72610e3f6861c381b71c6004
Folareen <sakawahab03@gmail.com>
2023-06-07 06:24:23 +0100

    added authSlice

--- app/src/redux/store.ts
+++ app/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './features/cartSlice'
+import authReducer from './features/authSlice'
 
 const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        auth: authReducer
     }
 })
 
