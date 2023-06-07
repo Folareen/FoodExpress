922f454f4864856964bb50176168f25c4b418a3f
Folareen <sakawahab03@gmail.com>
2023-06-07 15:49:20 +0100

    changed baseUrl to live url

--- app/src/config/api.ts
+++ app/src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = 'http://192.168.231.156:5000/api/v1'
+const BASE_URL = 'https://foodexpress-aal8.onrender.com/api/v1'
 
 const api = axios.create({
     baseURL: BASE_URL,
